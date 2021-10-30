06cb7b4b3a54a8d6549de6841a673cda13e36419
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-10-30 23:32:48 +0530

    update : set todos after render

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,62 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Box from "@mui/system/Box";
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoModal from "./components/TodoModal/TodoModal";
+import ModalBg from "./components/ModalBg/ModalBg";
+import { useTodos } from "./contexts/todo-context";
+import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { todos, dispatch, openTodoCreateModal } = useTodos();
+
+  const onCreateTaskClicked = () =>
+    dispatch({
+      type: "SHOW_TODO_CREATE_MODAL",
+      payload: openTodoCreateModal,
+    });
+
+  useEffect(() => {
+    (async function () {
+      const { data } = await axios.get("http://localhost:5555/todos");
+
+      if (data) {
+        dispatch({ type: "SET_TODOS", payload: { todos: data } });
+      }
+    })();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {openTodoCreateModal && (
+        <ModalBg>
+          <TodoModal />
+        </ModalBg>
+      )}
+
+      <Box p={2}>
+        <Button
+          onClick={onCreateTaskClicked}
+          variant="contained"
+          startIcon={<AddIcon />}
+        >
+          Create Task
+        </Button>
+
+        {todos.length > 0 && (
+          <Box mt={2}>
+            {[...todos]
+              .sort((a, b) => b.id - a.id)
+              .map((todo) => (
+                <TodoItem key={todo.id} todo={todo} />
+              ))}
+          </Box>
+        )}
+      </Box>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
