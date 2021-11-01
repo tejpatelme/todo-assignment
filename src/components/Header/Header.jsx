e408c5ed1364d4c1a1112e2413b9f7629a8bcf79
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-11-01 11:46:39 +0530

    style : add bottom margin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,10 @@ import { Typography } from "@mui/material";
 
 export default function Header() {
   return (
-    <Typography variant="h4" sx={{ fontWeight: 700, textAlign: "center" }}>
+    <Typography
+      variant="h4"
+      sx={{ fontWeight: 700, textAlign: "center", marginBottom: "2rem" }}
+    >
       TODO LIST
     </Typography>
   );
