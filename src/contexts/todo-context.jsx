61995ea8d439c1d2b907a572a2dfef680a875ffa
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-11-01 12:12:07 +0530

    chore : remove unused imports

--- src/contexts/todo-context.jsx
+++ src/contexts/todo-context.jsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useState,
-} from "react";
+import { createContext, useContext, useReducer } from "react";
 import todoReducer from "../reducers/todo-reducer";
 
 const TodoContext = createContext();
